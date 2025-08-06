76c9b0ccdd71f6ad6340cb2866b5bffc58e7c1ba
Võ Đăng Khoa <133868589+KhoaVo0911@users.noreply.github.com>
2025-08-06 15:37:44 +0700

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://figmafolio.com",
+  site: "https://app.figmafolio.com",
   integrations: [mdx(), sitemap(), icon(), tailwind()],
   vite: {
     resolve: {
